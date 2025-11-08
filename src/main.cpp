bfe970c1dc7993062c1f99316583e1314e54ef9a
Dany <Dany@DESKTOP-LR3MLCS>
2025-11-09 00:11:51 +0330

    add basic Mat test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <opencv2/core.hpp>
 
 int main()
 {
-    std::cout << "Hekko World" << '\n' ;
+    cv::Mat M(4, 4, CV_8UC1);
+    cv::randu(M, 0, 255);
+    std::cout << "M = " << '\n' << " " << M << '\n' << '\n';
+    std::cout << "Hello World" << '\n' ;
     return 0;
 }
